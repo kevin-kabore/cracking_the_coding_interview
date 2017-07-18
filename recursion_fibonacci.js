ce4f6e9f4c803d7e71770ae0882eee012441a396
Kevin Kabore <kevin.s.kabore@gmail.com>
2017-07-18 13:53:52 -0700

    completes fibonacci series recursively

---
+++ recursion_fibonacci.js
@@ -0,0 +1,12 @@
+function fibonacci(n) {
+    var total = 0;
+    // base case
+    if(n === 0) {
+        return total += n;
+    } else if (n === 1) {
+        return total += n;
+    } else { // recursive case
+        return total = fibonacci(n-1) + fibonacci(n-2)
+    }
+    console.log(total)
+}
