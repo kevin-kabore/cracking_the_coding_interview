45655f386387cab0f0b46053f7bb995a32bbc9f7
Kevin Kabore <kevin.s.kabore@gmail.com>
2017-07-21 15:20:25 -0700

    completes stacks challenge

---
+++ stacks_balancedBrackets.js
@@ -0,0 +1,35 @@
+function main() {
+    var t = parseInt(readLine());
+    function validator(string) {
+        let openersClosers = {
+            '{' : '}',
+            '(' : ')',
+            '[' : ']'
+        }
+        let openers = ['{', '(', '[']
+        let closers = ['}', ')', ']']
+        let arr = []
+
+        for(var i = 0; i < string.length; i++) {
+            let char = string[i]
+            if(openers.includes(char)) {
+                arr.push(char)
+            } else if (closers.includes(char)) {
+                if(!arr.length) { //if there is a closer with no opener in the arr must be false
+                    return console.log("NO")
+                }
+                let lastOpener = arr.pop();
+                if(openersClosers[lastOpener] !== char) { //if val of last opener as key doesn't match closer
+                    return console.log("NO");
+                }
+            }
+        }
+        if(arr.length === 0) { // if all openers were removed when closers found
+            return console.log('YES')
+        }
+    }
+    for(var a0 = 0; a0 < t; a0++){
+        var expression = readLine();
+        validator(expression)
+    }
+}
