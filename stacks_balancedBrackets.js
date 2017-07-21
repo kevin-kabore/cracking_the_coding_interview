3b53bed148779b1aa0c92c29287d06f8cb973c38
Kevin Kabore <kevin.s.kabore@gmail.com>
2017-07-21 15:42:44 -0700

    completes last check

--- stacks_balancedBrackets.js
+++ stacks_balancedBrackets.js
@@ -17,17 +17,19 @@ function main() {
             } else if (closers.includes(char)) {
                 if(!arr.length) { //if there is a closer with no opener in the arr must be false
                     return console.log("NO")
-                }
-                let lastOpener = arr.pop();
-                if(openersClosers[lastOpener] !== char) { //if val of last opener as key doesn't match closer
-                    return console.log("NO");
+                } else {
+                    let lastOpener = arr.pop();
+                    if(openersClosers[lastOpener] !== char) { //if val of last opener as key doesn't match closer
+                        return console.log("NO");
+                    }
                 }
             }
         }
         if(arr.length === 0) { // if all openers were removed when closers found
             return console.log('YES')
-        }
+        } else {return console.log('NO')}
     }
+
     for(var a0 = 0; a0 < t; a0++){
         var expression = readLine();
         validator(expression)
